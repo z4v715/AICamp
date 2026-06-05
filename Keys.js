549562b01687804e3c5b56ce7ebd851c9d7b2a87
Z4V <aidenazavaleta@gmail.com>
2026-06-05 10:06:50 -0500

    Create Keys.js

---
+++ Keys.js
@@ -0,0 +1,7 @@
+export const hugging_face_key = '<token_here>'
+export const cloudinaryCloudname = "dmm8zr0az"
+export const sightengine_api_user = "491630136"
+export const sightengine_api_secret = "C4MoxqQPspV7jVvMv728pcy2sTpjDe9A"
+export const sightengine_workflow = "wfl_g5fdiHz4maLRX7ezhd867"
+export const sightengine_listID = "tli_g5t6wWqDfOy6QW00gUBbQ"
+export const elevenlabs_api_key = "<token_here>"
